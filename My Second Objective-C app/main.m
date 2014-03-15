4304dc09cca3943899a3e9880c6bc4a614bb86d3
fgsantoyo <fgsantoyo@gmail.com>
2014-03-15 17:12:55 -0400

    Initial Commit

---
+++ My Second Objective-C app/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  My Second Objective-C app
+//
+//  Created by Fernando Garcia, Jr on 3/15/14.
+//  Copyright (c) 2014 Fernando Garcia, Jr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
